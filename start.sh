2e45f90b322d8389e2da982652e96d4b5de6ebd6
Олег <osumsky@gmail.com>
2022-02-09 09:26:19 +0200

    add start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 sudo docker build -t server:0.1 -f ./Dockerfile-dev .
 
-sudo docker run -dit -p 8080:3000 -v $(pwd):/app:ro --name server_docker server:0.1
+# Check if server_docker are present (run or paused)
+if sudo docker ps -a | grep server_docker; 
+then 
+  sudo docker start -i server_docker
+else
+  sudo docker run -dit -p 8080:3000 -v $(pwd):/app:ro --name server_docker server:0.1
+fi
